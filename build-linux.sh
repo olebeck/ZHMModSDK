f4341e1131228b06f273fd51cbac7442acdb9db3
olebeck <31539311+olebeck@users.noreply.github.com>
2022-12-31 00:12:59 +0100

    lower case dxc.exe

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,6 @@
 conf=$1
 dxc_folder=_build/x64-${conf}-Mingw/vcpkg_installed/x64-linux/tools/directx-dxc
 chmod +x ${dxc_folder}/dxc.exe
-cp ${dxc_folder}/dxc.exe ${dxc_folder}/DXC.EXE
 
 cmake --preset x64-${conf}-Mingw .
 cmake --build _build/x64-${conf}-Mingw 
