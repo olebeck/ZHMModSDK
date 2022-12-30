04d03ecae9b7d65bee8635bb0956a6a3fcfacd9a
olebeck <31539311+olebeck@users.noreply.github.com>
2022-12-30 23:37:25 +0100

    fix patch crlf

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,3 +1,4 @@
 cat imgui-dx12-mingw.patch | patch -d ../External/imgui/source -p1 -N -r /dev/null
-cat directxtk12-wine-ignore-errors.patch | patch -d ../External/DirectXTK12 -p1 -N -r /dev/null
+cat directxtk12-ignore-shader-errors.patch | patch -d ../External/DirectXTK12 -p1 -N -r /dev/null --binary
+cat directxtk12-wine.patch | patch -d ../External/DirectXTK12 -p1 -N -r /dev/null
 cat vcpkg-patch-support-linux.patch | patch -d ../External/vcpkg -p1 -N -r /dev/null
